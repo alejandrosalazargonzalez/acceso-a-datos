49ffbabb2c31d9c1d54a4bc2fb09e775f602659c
Salazar <115075962+alejandrosalazargonzalez@users.noreply.github.com>
2025-09-28 19:41:47 +0100

    se sube ejercicio2.php

---
+++ unidad-1/practica-ficheros/ejercicio2.php
@@ -0,0 +1,24 @@
+<?php
+function recogerPalabras(): array {
+    $texto = strtolower(file_get_contents("texto.txt"));
+    preg_match_all("/\p{L}+/u", $texto, $words);
+    return $words[0];
+}
+
+function resultado(): void {
+    if (!$fp = fopen("estadisticas.csv", "w")) {
+        echo "no se puede abrir el archivo";
+        return;
+    }
+
+    $words = recogerPalabras();
+    $ocurrencias = array_count_values($words);
+
+    fwrite($fp, "palabra,frequencia\n");
+    foreach ($ocurrencias as $word => $frequency) {
+        fwrite($fp, "$word,$frequency\n");
+    }
+    fclose($fp);
+}
+
+resultado();
