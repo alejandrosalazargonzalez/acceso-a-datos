b12755f3712d9cee583bd8334bb75b4e03a511ee
alejandrosalazargonzalez <alejandrosalazargonzalez2004@gmail.com>
2025-10-01 18:39:23 +0100

    se agrega la carpeta mi_proyecto

---
+++ unidad-1/mi_proyecto/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '6aa387fcf53e97e02c8cf7bbb694ed661bd6bd67',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '6aa387fcf53e97e02c8cf7bbb694ed661bd6bd67',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
